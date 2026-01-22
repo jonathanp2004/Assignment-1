703b3f1b4726fb1fed1a6d5421a6582d42e00304
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-01-22 14:03:06 -0600

    finished longestCommonSubstring method

--- src/Main.java
+++ src/Main.java
@@ -28,10 +28,31 @@ public class Main {
         int a = fText.length();
         int b = sText.length();
 
-        int [][] ta = new int[a+1][b+1];
+        int[][] ta = new int[a+1][b+1];
 
+        int maxLength = 0;
+        int endIndex= 0;
 
+        for(int i =1; i <= a; i++){
+            for(int j = 1; j <= b; j++){
 
+                if(fText.charAt(i - 1) == sText.charAt(j - 1))
+                {
+                    ta[i][j] = ta[i-1][j-1]+1;
+
+                    if(ta[i][j] > maxLength)
+                    {
+                        maxLength = ta[i][j];
+                        endIndex = i;
+                    } else {
+                        ta[i][j] = 0;
+                    }
+                }
+            }
+            if(maxLength == 0) {
+                return "";
+            }
+        }  return fText.substring(endIndex - maxLength, endIndex);
     }
 
 }
