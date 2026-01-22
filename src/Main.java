8f26c4c1af7d57b98cff97b6a05ee94a46368afa
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-01-21 23:15:06 -0600

    Added First longestCommonSubsequence Method and started longestCommonString method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
 
 
-    public int
+    public int longestCommonSubsequence(String firstText, String secondText){
+
+        int a = firstText.length();
+        int b = secondText.length();
+
+        int [][] table = new int[a +1][b + 1];
+
+        for(int i = 1; i <= a; i++){
+            for(int j = 1; j <= b; j++ )
+            {
+                if(firstText.charAt(i-1) == secondText.charAt(j-1))
+                {
+                    table [i][j] = table[i-1][j-1]+1;
+                } else{
+                    table [i][j] = Math.max(table[i-1][j], table[i][j-1]);
+                }
+            }
+        }
+
+        return table[a][b];
+    }
+
+    public static String longestCommonSubstring(String fText, String sText){
+
+        int a = fText.length();
+        int b = sText.length();
+
+        int [][] ta = new int[a+1][b+1];
+
+
+
+    }
+
 }
